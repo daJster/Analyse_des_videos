dbe17b7b81cc67da3c577dd95d3ed2554a59bd1c
Jad Elkarchi <elkarchie2@gmail.com>
2024-01-24 20:54:41 +0100

    add vgg19 and train and test tensors

--- GITW_light3_TD/train.py
+++ GITW_light3_TD/train.py
@@ -3,12 +3,14 @@ import cv2
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset, DataLoader, TensorDataset
 import numpy as np
 from torchvision import transforms
 from PIL import Image
 from torchvision.utils import save_image
 import torchvision.transforms.functional as F
+from torchvision import models, transforms
+from sklearn.model_selection import train_test_split
 
 class CustomDataset(Dataset):
     def __init__(self, data_dir):
@@ -17,10 +19,11 @@ class CustomDataset(Dataset):
         self.video_paths = []
         self.bboxes_paths = []
         self.saliency_maps_paths = []
+        self.obj_types = []
         self.load_data()
 
     def load_data(self):
-        for cls in self.classes:
+        for icls, cls in enumerate(self.classes):
             cls_path = os.path.join(self.data_dir, cls)
             video_dirs = os.listdir(cls_path)
             for video_dir in video_dirs:
@@ -37,6 +40,7 @@ class CustomDataset(Dataset):
                 
                 self.video_paths.append(video_path)
                 self.bboxes_paths.append(bbox_path)
+                self.obj_types.append(icls)
                 self.saliency_maps_paths.append(saliency_path)
                 
                 
@@ -107,54 +111,112 @@ def train_model(train_loader, model, criterion, optimizer, num_epochs=10):
             optimizer.step()
         print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item()}')
 
-def main():
-    data_dir = 'train'
+
+def get_dataset(data_dir):
     dataset = CustomDataset(data_dir)
 
-    output_dir = 'train_dataset'
-    os.makedirs(output_dir, exist_ok=True)
-
+    list_all_frames = []
+    list_all_types = []
     for idx in range(dataset.__len__()):
         video_path = dataset.video_paths[idx]
         bbox_path = dataset.bboxes_paths[idx]
+        obj_type = dataset.obj_types[idx]
         saliency_path = dataset.saliency_maps_paths[idx]
 
         saliency_applied = dataset.apply_saliency(video_path, bbox_path, saliency_path)
-        unique_name = video_path.split("/")[1] + '_' + os.path.splitext(os.path.basename(video_path))[0] + '_saliency'
+
+        list_all_frames.extend(save_frames(saliency_applied))
+        list_all_types.extend([obj_type] * len(saliency_applied))
         
-        # Resize frames to a common size
-        common_size = (356, 356)  # Choose the desired size
-        # resized_frames = [torch.nn.functional.interpolate(frame.unsqueeze(0), size=common_size, mode='bilinear', align_corners=False).squeeze(0) for frame in saliency_applied]
-        # Save the resized frames as a video
-        video_output_path = os.path.join(output_dir, f'{unique_name}_saliency_applied_video.mp4')
-        # Save the resized frames as PNG images
-        save_frames(saliency_applied, os.path.join(output_dir), unique_name)
+        
+    largest_shape = max(image.shape for image in list_all_frames)
+    resized_images = [np.full_like(np.zeros(largest_shape), 255, dtype=np.uint8) for _ in range(len(list_all_frames))]
+    for i, image in enumerate(list_all_frames):
+        resized_images[i][:image.shape[0], :image.shape[1], :] = image
+        
+    # Convert resized_images to PyTorch tensors
+    resized_tensors = torch.stack(torch.tensor(image.transpose(2, 0, 1), dtype=torch.float32) for image in resized_images)
+    classes_tensor = torch.tensor(list_all_types, dtype=torch.long)
+    
+    # Create a TensorDataset from image tensors and labels
+    dataset = TensorDataset(resized_tensors, classes_tensor)
+    
+    return dataset
+
+def main():
+    train_dataset, val_dataset = get_dataset('train'), get_dataset('test')
+    
+    batch_size = 32
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+    
+    # Load pre-trained VGG-19 model
+    vgg19_model = models.vgg19(pretrained=True)
+    num_classes = len(set(train_dataset[1]))
+    vgg19_model.classifier[6] = nn.Linear(4096, num_classes)
+    
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(vgg19_model.parameters(), lr=0.001, momentum=0.9)
 
 
+    # Training loop
+    num_epochs = 10
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    vgg19_model.to(device)
 
-def save_frames(frames, output_path, unique_name, fps=30):
-    os.makedirs(output_path, exist_ok=True)
+    for epoch in range(num_epochs):
+        vgg19_model.train()
+        for inputs, labels in train_loader:
+            inputs, labels = inputs.to(device), labels.to(device)
+            optimizer.zero_grad()
+            outputs = vgg19_model(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+        # Validation loop
+        vgg19_model.eval()
+        val_loss = 0.0
+        correct = 0
+
+        with torch.no_grad():
+            for inputs, labels in val_loader:
+                inputs, labels = inputs.to(device), labels.to(device)
+                outputs = vgg19_model(inputs)
+                loss = criterion(outputs, labels)
+                val_loss += loss.item()
+                _, predicted = torch.max(outputs, 1)
+                correct += (predicted == labels).sum().item()
+
+        val_loss /= len(val_loader.dataset)
+        accuracy = correct / len(val_loader.dataset)
+
+        print(f"Epoch {epoch + 1}/{num_epochs}, Validation Loss: {val_loss:.4f}, Accuracy: {accuracy:.4f}")
+
+def save_frames(frames):
+    # os.makedirs(output_path, exist_ok=True)
 
     # Video writer setup
-    video_path = os.path.join(output_path, f'{unique_name}_video.mp4')
-    height, width, _ = frames[0].shape
-    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-    video_writer = cv2.VideoWriter(video_path, fourcc, fps, (width, height))
-
+    # video_path = os.path.join(output_path, f'{unique_name}_video.mp4')
+    # height, width, _ = frames[0].shape
+    # fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    # video_writer = cv2.VideoWriter(video_path, fourcc, fps, (width, height))
+    
+    list_frames = []
     for frame_idx, frame in enumerate(frames):
-        frame_path = os.path.join(output_path, f'{unique_name}_frame_{frame_idx}.png')
         print(frame_idx, end="\r")
         # Convert torch tensor to NumPy array
         numpy_array = (frame.permute(1, 2, 0).cpu().numpy() * 255).astype('uint8')
 
         # Transpose the dimensions to make it (3, 222, 162)
         numpy_array = numpy_array.transpose(2, 0, 1)
-
+        list_frames.append(numpy_array)
         # Write the frame to the video
-        video_writer.write(numpy_array)
+        # video_writer.write(numpy_array)
 
     # Release the video writer
-    video_writer.release()
+    # video_writer.release()
+    return list_frames
             
             
 def merge_videos(input_dir, output_path):
